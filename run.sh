66fbcc15e2a3eb5413353dc0ae48aa627d671746
schoolgaminghd <testaccontimarobot@gmail.com>
2022-04-29 13:39:56 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@
 cd stable-download/bukkit-command
 java -Xmx512M -Xms512M -jar craftbukkit-1.5.2-R1.0.jar
 cd ..
+cd stable-download
 cd stable-download/bungee-command
 java -Xmx32M -Xms32M -jar bungee-dist.jar
 cd ..
